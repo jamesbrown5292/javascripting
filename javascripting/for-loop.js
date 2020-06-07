dd9bb5238e813223bd0bb81888d9bf4a192ec78e
James Brown <jamesbrown5292@gmail.com>
2020-06-07 05:40:14 +0000

    Initial commit of completed javascripting tutorial exercises

---
+++ javascripting/for-loop.js
@@ -0,0 +1,3 @@
+let limit = 10
+let total = 0
+for (i=0; i<limit; i++) { total += i } console.log(total)
---
+++ javascripting/function-arguments.js
@@ -0,0 +1,5 @@
+function math (x, y, z) {
+let total = (y*z)+x
+return total
+}
+console.log(math(53, 61, 67))
